3f8295b6b9f90c4ec03673c1ffb7d3af7f57db96
Benjamin <shallifi@hotmail.com>
2023-02-21 22:36:38 -0500

    added dummy info using rails console. need to fix value of employee model

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/hello', to: 'application#hello_world'
-
+# get '/employees', to: ''
  
 end
