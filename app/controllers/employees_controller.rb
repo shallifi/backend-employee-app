da71ab7d49d8e68b4f01bb4d3b42a945df858927
Benjamin <shallifi@hotmail.com>
2023-04-18 22:22:46 -0400

    changed cors rb to include origin of 4000

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,5 @@
 class EmployeesController < ApplicationController
-  before_action :set_employee, only: %i[ show update destroy ]
+  # before_action :set_employee, only: %i[ show update destroy ]
 
   # GET /employees
   def index
